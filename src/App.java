b131c513a93d22ecf6da1663c540f05e730a073e
blyx <brianlimyuxi@gmail.com>
2026-07-27 18:35:40 +0800

    javafx setup

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Button btn = new Button("click me cuz y not");
+
+        btn.setOnAction(e -> {
+            System.out.println("nice");
+        });
+
+        VBox root = new VBox(10);
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 200);
+
+        stage.setTitle("Solar System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
